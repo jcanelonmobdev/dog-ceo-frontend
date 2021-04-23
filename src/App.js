0073717a217ca210649eeccba495fd030cf33ecf
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-04-23 01:33:33 -0400

    Integrar React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './presentation/pages/HomePage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
